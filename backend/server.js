186e832c1e149580af17bb95f34c72277caca738
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2023-11-29 09:05:01 +0100

    - connection config - set user controller functions - add userModel (with methods) and set bcrypt - generateToken Function - add user routes - config authMiddleware

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
+import cookieParser from "cookie-parser";
 
 const port = process.env.port || 5000;
 
 import userRoutes from "./routes/userRoutes.js";
 
+connectDB();
+
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
